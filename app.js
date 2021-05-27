8bfd886dcf6b6811dbdd04f514e0824f2dc9014b
Jennesy <jennesy.dai@gmail.com>
2021-05-27 23:45:19 +0800

    feat: add handlebars & add home page

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
 
 const app = express()
 const port = 3000
 
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
+//routes setting
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
-  res.send('This is a URL Shortener website')
+  return res.render('index')
 })
 
 app.listen(port, () => {
