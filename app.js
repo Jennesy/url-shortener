5f1c72edbf918f94d544a03fb4b0a93cfa09ed3c
Jennesy <jennesy.dai@gmail.com>
2021-05-28 19:45:46 +0800

    feat: add generate URL function

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+const generateURL = require('./models/generateURL')
+require('./config/mongoose')
 
 const app = express()
 const port = 3000
@@ -7,11 +10,18 @@ const port = 3000
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+app.use(bodyParser.urlencoded({ extended: true }))
+
 //routes setting
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
   return res.render('index')
+})
+app.post('/', async (req, res) => {
+  const routes = await generateURL(req.body.originalURL)
+  const shortenURL = `${req.protocol}://${req.get('host')}/${routes}`
+  res.render('result', { shortenURL })
 })
 
 app.listen(port, () => {
---
+++ models/generateURL.js
@@ -0,0 +1,36 @@
+const URL = require('./url')
+const characters = 'abcdefghijklmnopqrstuvwxyz0123456789'
+const collection = characters.split('')
+
+function sample(array) {
+  const index = Math.floor(Math.random() * array.length)
+  return array[index]
+}
+
+function isValid(routes) {
+  return URL.find({ shortenURL: routes }).lean()
+    .then(url => {
+      return !Boolean(url.length)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+}
+
+async function generateURL(originalURL) {
+  let routes = ''
+  for (let i = 0; i < 8; i++) {
+    routes += sample(collection)
+  }
+  if (await isValid(routes)) {
+    console.log('isValid')
+    URL.create({ originalURL, shortenURL: routes })
+      .then(url => { console.log(`${url} saved`) })
+      .catch(error => console.log(error))
+    return routes
+  } else {
+    generateURL(originalURL)
+  }
+}
+
+module.exports = generateURL
